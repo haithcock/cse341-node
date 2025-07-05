8276952b759aa443d3fbf06939c51e0df7254151
haithcock <dakota.haith1997@.com>
2025-07-05 13:17:23 -0400

    testing

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ app.use(errors.logRequestUrl); // Logs every request
 app.use('/', mainRoutes); // Main routes
 app.use(errors.handleNotFound); // 404 fallback
 app.use(errors.handleErrors);   // Error handler
-
+/*
+testing
+*/
  
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
