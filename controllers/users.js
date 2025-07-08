2fd99a2040dffe56fb80fd96bd16bfb8f7901e76
haithcock <dakota.haith1997@.com>
2025-07-08 14:28:39 -0400

    working on making contacts

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
+
 const getAll = async (req, res) => {
 
     const result = await mongodb.getDatabase().db().collection('users').find();
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,16 @@
+//this file is located at: /home/haithcock/Desktop/cse341/cse341-node/routes/users.js
+
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Users = require('../models/users');
+
 
 const usersController = require('../controllers/users');
+
+
+
+
 
 router.get('/', usersController.getAll);
 router.get('/:id', usersController.getSingle);
