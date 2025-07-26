7e547f3ba026c21414fe2ac02b9e5c7a43b87139
haithcock <dakota.haith1997@.com>
2025-07-26 12:00:17 -0400

    edited gitignore

---
+++ swagger.js
@@ -0,0 +1,27 @@
+const swaggerAutogen = require('swagger-autogen')({ openapi: '3.0.0' });
+
+const doc = {
+  info: {
+    version: "1.0.0",
+    title: "Users API",
+    description: "API for managing users"
+  },
+  host: "localhost:3000",
+  basePath: "/",
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+      name: "Users",
+      description: "User management endpoints"
+    }
+  ]
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./routes/users.js'];
+
+if (process.env.NODE_ENV !== 'production') {
+  swaggerAutogen(outputFile, endpointsFiles, doc);
+}
